5535cec244eb36455109bc565d6b924138290f62
William Cooper <willcooper73@gmail.com>
2018-08-25 14:25:16 +0100

    Version 1.0.0 - Initial commit
    Signed-off-by: William Cooper <willcooper73@gmail.com>

---
+++ Schedule01/frmMain.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Schedule01
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tmeTima = new System.Windows.Forms.DateTimePicker();
+            this.cboWeekday = new System.Windows.Forms.ComboBox();
+            this.lstSlots = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tmeTima
+            // 
+            this.tmeTima.CustomFormat = "hh/ss";
+            this.tmeTima.Location = new System.Drawing.Point(140, 12);
+            this.tmeTima.Name = "tmeTima";
+            this.tmeTima.Size = new System.Drawing.Size(200, 20);
+            this.tmeTima.TabIndex = 0;
+            this.tmeTima.Value = new System.DateTime(2018, 8, 25, 13, 48, 59, 0);
+            // 
+            // cboWeekday
+            // 
+            this.cboWeekday.FormattingEnabled = true;
+            this.cboWeekday.Items.AddRange(new object[] {
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wedneday",
+            "Thursday",
+            "Friday",
+            "Saturday"});
+            this.cboWeekday.Location = new System.Drawing.Point(13, 10);
+            this.cboWeekday.Name = "cboWeekday";
+            this.cboWeekday.Size = new System.Drawing.Size(121, 21);
+            this.cboWeekday.TabIndex = 1;
+            // 
+            // lstSlots
+            // 
+            this.lstSlots.FormattingEnabled = true;
+            this.lstSlots.Location = new System.Drawing.Point(13, 38);
+            this.lstSlots.Name = "lstSlots";
+            this.lstSlots.Size = new System.Drawing.Size(367, 277);
+            this.lstSlots.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(71, 321);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(152, 321);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(233, 320);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(112, 350);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(193, 350);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 7;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 384);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstSlots);
+            this.Controls.Add(this.cboWeekday);
+            this.Controls.Add(this.tmeTima);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker tmeTima;
+        private System.Windows.Forms.ComboBox cboWeekday;
+        private System.Windows.Forms.ListBox lstSlots;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
